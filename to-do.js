46c88528ad7a408b90366483d2de077b0e56722d
prasannanjali <prasannanjalipuchakayala@gmail.com>
2026-08-03 22:43:32 +0530

    Update to-do.js

--- to-do.js
+++ to-do.js
@@ -1,6 +1,18 @@
+
+
+ let total = 0;
+ let pending = 0;
+ let completed =0;
+
 const input = document.getElementById("taskInput");
 const button = document.getElementById("btn-1");
 const tasklist = document.getElementById("taskList");
+
+function updateTotal(){
+    document.getElementById("Total").innerHTML = "Total : "+ total;
+    document.getElementById("Completed").innerHTML = "Completed : "+ completed;
+    document.getElementById("Pending").innerHTML = "Pending : "+ pending;
+}
 
 
 button.addEventListener("click" , myFunction);
@@ -9,24 +21,48 @@ function myFunction(){
 
  let userInput = input.value; 
  let li=document.createElement("li");//cretated the userInput 
+ 
 
 // for userInput when it click on addtask it should come with checkbox
  let checkbox = document.createElement("input");
  checkbox.type = "checkbox";
  li.appendChild(checkbox);//add checkbox
-
- 
  let text=document.createTextNode(" " + userInput);//createTextNode is used to keep together the checkbox and taask
  li.appendChild(text);//add text after checkbox
 
- 
- tasklist.appendChild(li);  //add li to ul
 
+// all about delete button added when the user create the the task and click on add task
+ let del = document.createElement("button");
+ del.type = "button";
+ del.innerText = "Delete"
+// when click on delete button the task should get deleted.
+ del.onclick=function(){
+    li.remove();
+
+    total--;
+
+    if (checkbox.checked){
+        pending--;
+        completed++;
+    }else{
+        completed--;
+        pending++;
+    }
+    updateTotal();
+ };
+
+ 
+
+li.appendChild(document.createTextNode(" "));
+li.appendChild(del);
+tasklist.appendChild(li); //add li to ul
+
+
+total++;
+pending++;
+updateTotal();
+
+ 
  input.value="";
 
 }
-
-//for userInput when it click on addtask it should come with checkbox.----done
-// when the user click the checkbox it marks as (rightmark) then it should get deleted
-//total is always increased when the add task button is clicked
-//completed also should increase when the checkbox mark as(rightmark) and pending should be decreased.
